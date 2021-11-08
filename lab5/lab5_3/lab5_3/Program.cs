6ce8d167cae8199934ab94f2c143b1f94febd4de
Admin <Admin@BEST-KOMP>
2021-11-08 12:22:50 +0300

    'laba5'

---
+++ lab5/lab5_3/lab5_3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab5_3
+{
+    public class SaltedHash
+    {
+        public static byte[] GenerateSalt()
+        {
+            const int saltLen = 32;
+
+            using (var ranNumGen = new RNGCryptoServiceProvider())
+            {
+                var randomNum = new byte[saltLen];
+                ranNumGen.GetBytes(randomNum);
+                return randomNum;
+            }
+        }
+
+        private static byte[] Combine(byte[] first, byte[] second)
+        {
+            var rett = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, rett, 0, first.Length);
+            Buffer.BlockCopy(second, 0, rett, first.Length, second.Length);
+            return rett;
+        }
+
+        public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Combine(toBeHashed, salt));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string pass = "KCML2015_2021as";
+            byte[] salt = SaltedHash.GenerateSalt();
+            Console.WriteLine("Pass - " + pass);
+            Console.WriteLine("Salt - " + Convert.ToBase64String(salt));
+            Console.WriteLine();
+            var hashedPassword1 = SaltedHash.HashPasswordWithSalt(Encoding.UTF8.GetBytes(pass), salt);
+            Console.WriteLine("Hashed Password - " + Convert.ToBase64String(hashedPassword1));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab5/lab5_4/lab5_4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab5_4
+{
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, System.Security.Cryptography.HashAlgorithmName hashAlgorithm)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, hashAlgorithm))
+            {
+                return rfc2898.GetBytes(20);
+            }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string passwordToHash = "gogogogo";
+            HashPassword(passwordToHash, 10000);
+            HashPassword(passwordToHash, 60000);
+            HashPassword(passwordToHash, 110000);
+            HashPassword(passwordToHash, 160000);
+            HashPassword(passwordToHash, 210000);
+            HashPassword(passwordToHash, 260000);
+            HashPassword(passwordToHash, 310000);
+            HashPassword(passwordToHash, 360000);
+            HashPassword(passwordToHash, 410000);
+            HashPassword(passwordToHash, 460000);
+            Console.ReadLine();
+        }
+        private static void HashPassword(string passwordToHash, int numberOfRounds)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var hashedPassword = PBKDF2.HashPassword(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numberOfRounds, HashAlgorithmName.SHA512);
+            sw.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Password to hash : " + passwordToHash);
+            Console.WriteLine("Hashed Password : " + Convert.ToBase64String(hashedPassword));
+            Console.WriteLine("Iterations " + numberOfRounds + " Used Time: " + sw.ElapsedMilliseconds + "ms");
+        }
+    }
+}
