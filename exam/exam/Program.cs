036460028260afff733ec1a1d04ee11e06a37d25
denisov <thedenike@gmail.com>
2021-12-23 11:12:56 +0200

    exam

---
+++ exam/exam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Project
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+           
+            RandomNumberGenerator rng = new RNGCryptoServiceProvider();
+            
+            byte[] nums = new byte[15];
+            
+            rng.GetBytes(nums);
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
